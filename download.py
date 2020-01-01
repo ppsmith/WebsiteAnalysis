cb44051b4e7ea4657270e4f7c0d8b9f902a9ee86
ppsmith <54873198+ppsmith@users.noreply.github.com>
2020-01-01 11:25:10 -0500

    Cleaned up code style.

--- download.py
+++ download.py
@@ -4,56 +4,53 @@ import createdict
 import csv
 import bs4
 
-#contains methods to dolwnload html of known webpages, or a specifc webpage
 
+# contains methods to download html of known web pages, or a specific webpage
 
-#url = ["https://www.nytimes.com/section/business/economy", "https://www.nytimes.com/section/business/", "https://nyt.com/", "https://www.wsj.com/", "https://www.wsj.com/news/world",
-       #"https://www.wsj.com/news/economy", "https://www.wsj.com/news/business"]  #starting urls. Keep in mind that this uis not the curretn state of URLs
-
-
-#func. downloads the data for all links in the dictionary. path is the path for the dictionary, and the dirtyRoot is the file for the dirty data. From seed is an option. 1 if the dict is emtpy, 0 if the dict has entries
-def downLoadfromdict(path, dirtyRoot, fromSeed = 1):
+# func. downloads the data for all links in the dictionary. path is the path for the dictionary, and the dirtyRoot is
+# the file for the dirty data. From seed is an option. 1 if the dict is emtpy, 0 if the dict has entries
+def downLoadfromdict(path, dirtyRoot, fromSeed=1):
     try:
-        if(fromSeed == 1):
-            dictionary = createdict.create(path)  #creates an instanse of a new dictionary for links
+        if (fromSeed == 1):
+            dictionary = createdict.create(path)  # creates an instanse of a new dictionary for links
 
             dictionary.createCSV()
 
-            csv_file = open(dictionary.getCSVPath())  #opens the file specified above
+            csv_file = open(dictionary.getCSVPath())  # opens the file specified above
 
-            reader = csv.reader(csv_file)  #creates a reader to read the cile opened above
+            reader = csv.reader(csv_file)  # creates a reader to read the cile opened above
 
-        if(fromSeed == 0):
+        if fromSeed == 0:
             csv_file = open(path, 'r')
             reader = csv.reader(csv_file)
 
-        for entry in reader:  #for each row in the dict
-            if entry is None:  #ensure that row exists
+        for entry in reader:  # for each row in the dict
+            if entry is None:  # ensure that row exists
                 return
 
             try:
-                http = PoolManager()  #start a pool manager
+                http = PoolManager()  # start a pool manager
                 s = ""
-                s = s.join(entry)  #s is a string version of the entry in the csv row
+                s = s.join(entry)  # s is a string version of the entry in the csv row
                 entry = s
                 print(entry)
-                data = http.request('GET', entry)  #request the webpage
+                data = http.request('GET', entry)  # request the webpage
 
-                newAddress = entry.replace('/', '')  #remove the slashes from the website url
+                newAddress = entry.replace('/', '')  # remove the slashes from the website url
                 newAddress = newAddress.replace('_', '')
                 newAddress = newAddress.replace('=', '')
                 newAddress = newAddress.replace('?', '')
 
-                newAddress = newAddress.strip('https://')  #remove the beggining of the url
+                newAddress = newAddress.strip('https://')  # remove the beggining of the url
 
-                filepath = dirtyRoot + newAddress + '.txt'  #make a new file path
+                filepath = dirtyRoot + newAddress + '.txt'  # make a new file path
 
-                file = open(filepath, 'w+')  #open new file
-                dataText = str(data.data)  #turn the data from the website into a string
-                file.write(entry)  #write the address the data is from into the first entry of the csv
-                file.write('\n')  #write a newline
+                file = open(filepath, 'w+')  # open new file
+                dataText = str(data.data)  # turn the data from the website into a string
+                file.write(entry)  # write the address the data is from into the first entry of the csv
+                file.write('\n')  # write a newline
                 soup = bs4.BeautifulSoup(dataText, 'html.parser')
-                file.write(soup.prettify())  #write the website data into the file
+                file.write(soup.prettify())  # write the website data into the file
             except Exception as e:
                 print(e)
                 continue
@@ -62,7 +59,7 @@ def downLoadfromdict(path, dirtyRoot, fromSeed = 1):
         print(e)
 
 
-#func. donwlaods the data from the link given. Path is the location of the local dictionary, link is the link to try to download, dirtyRoot is the location of the flle for diirty data
+# func. donwlaods the data from the link given. Path is the location of the local dictionary, link is the link to try to download, dirtyRoot is the location of the flle for diirty data
 def downLoadfromLink(path, link, dirtyRoot):
     try:
         http = PoolManager()
@@ -100,8 +97,7 @@ def downLoadfromLink(path, link, dirtyRoot):
         return e
 
 
-
-#download func. that dowloads when testLink is run. Does not need to download because testLink passes it the neccesary stuff
+# download func. that dowloads when testLink is run. Does not need to download because testLink passes it the neccesary stuff
 def downloadFromTestLink(data, link, dirtyRoot):
     try:
         newAddress = link.replace('/', '')
@@ -120,20 +116,20 @@ def downloadFromTestLink(data, link, dirtyRoot):
         print(e)
 
 
-#func. allows us to test the validity of any new link. Returns false is the link cannot be donwloaded or already is in the our system. Returns true if the link can be downloaded and the data added to the library
-def testLink(link, path, dirtyRoot, download = 1):
+# func. allows us to test the validity of any new link. Returns false is the link cannot be donwloaded or already is in the our system. Returns true if the link can be downloaded and the data added to the library
+def testLink(link, path, dirtyRoot, download=1):
     try:
         dict = open(path, 'r')
         reader = csv.reader(dict)
-        s =""
+        s = ""
         for entry in reader:
-            entry =  s.join(entry)
+            entry = s.join(entry)
             if link == entry:
                 return False
 
         http = PoolManager()
         data = http.request('GET', link)
-        if(download == 0):return True
+        if (download == 0): return True
         if len(data.data) < 10: return False
         downloadFromTestLink(data, link, dirtyRoot)
         return True
@@ -141,12 +137,13 @@ def testLink(link, path, dirtyRoot, download = 1):
         return False
 
 
-#func remvoes a link from the ditionary based on a file name.
+# func remvoes a link from the ditionary based on a file name.
 def removeFileFromList(file, dict):
     return None
 
-#func adds a link to the csvFile
-def addLinktoCSV(csvPath, link):  #csvPath is path to the csv file, and link is the link that needs to be added
+
+# func adds a link to the csvFile
+def addLinktoCSV(csvPath, link):  # csvPath is path to the csv file, and link is the link that needs to be added
     file = open(csvPath, 'a')
-    writer = csv.writer(file, lineterminator = '\n')
+    writer = csv.writer(file, lineterminator='\n')
     writer.writerow(link)
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import download
 import os
-import createSets
 import parse
 import addFromPage
 
-entries = os.listdir('C:\\Users\\ppsmith\\seekingAlphaData\\dirtyData\\')  #get the files in the dirty directory
+entries = os.listdir('C:\\Users\\ppsmith\\seekingAlphaData\\dirtyData\\')  # get the files in the dirty directory
 dirtyRoot = 'C:\\Users\\ppsmith\\seekingAlphaData\\dirtyData\\'
 cleanRoot = 'C:\\users\\ppsmith\\seekingAlphadata\\cleanData\\'
 
-dict = 'C:\\Users\\ppsmith\\seekingAlphaData\\dict.csv'  #path for the main dictionary of website urls
+dict = 'C:\\Users\\ppsmith\\seekingAlphaData\\dict.csv'  # path for the main dictionary of website urls
 
-print(download.downLoadfromdict(dict, dirtyRoot, 0))  #download the data from the websites in the dict to the dirtyData directory
+print(download.downLoadfromdict(dict, dirtyRoot,
+                                0))  # download the data from the websites in the dict to the dirtyData directory
 
-for entry in entries:  #for each entry, remove the html tags, and save the file to the cleanData directory
-    #print(addFromPage.add(entry, dict, dirtyRoot, dict))
+for entry in entries:  # for each entry, remove the html tags, and save the file to the cleanData directory
+    # print(addFromPage.add(entry, dict, dirtyRoot, dict))
     print(parse.parseSoup(dirtyRoot + entry, cleanRoot, dirtyRoot))
-
